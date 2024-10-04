dba339d76f537f51efe5b5e2601d9eb9c6603cf6
LirySouza <lirianysouza15@gmail.com>
2024-10-04 11:28:13 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HealthSolutions_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
