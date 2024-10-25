52d7225d72aff44d11f25de021d04e271ee3c8b4
LirySouza <lirianysouza15@gmail.com>
2024-10-25 11:19:55 -0300

    q

---
+++ Migrations/20241025115020_AgoraVai.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthSolutions_MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgoraVai : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
